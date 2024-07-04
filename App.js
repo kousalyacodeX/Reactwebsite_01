fbda9e491a41cb455e7c9090473c1649936d28ee
kousalya parmar <165292794+kousalyacodeX@users.noreply.github.com>
2024-07-04 16:27:51 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './Navbar';
+import Header from "./Header/Header";
+
+export const App = () => {
+  return (
+    <>
+    <Navbar/>
+    <Header/>
+
+    </>
+  );
+};
+export default App;
+
+
